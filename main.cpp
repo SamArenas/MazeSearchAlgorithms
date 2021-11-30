136236d8ab3180bc7a35d3caea8d130abe39e685
Samuel A <al2193001733@azc.uam.mx>
2021-11-30 15:51:00 -0600

    Added  generarHIjos method

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 #include <sys/time.h>
 using std::ifstream;
 
+std::pair<int,int> goal = {8,0}; //8,0
+std::pair<int,int> start = {7,12}; //7,12
+std::vector<std::vector<int>> maze;
+
 std::vector<std::vector<int>> llenarLaberinto(){
     ifstream indata; 
     indata.open("Practica 1 Maze.txt");
@@ -38,6 +42,21 @@ std::map<std::pair<int,int>, int> llenarCostos(){
     return costo;
 }
 
+void generarHijoUniforme(std::pair<int,int>& hijo, std::pair<int,int>& actual, auto& cola, std::map<std::pair<int,int>, int>& costo, std::map<std::pair<int,int>, std::pair<int,int>>& parents ){
+    if(hijo.first<0|| hijo.first>8 || hijo.second>14 || hijo.second<0){
+        return;
+    }
+
+    int gN = costo[actual] + maze[hijo.first][hijo.second];
+    std::cout<<"("<<hijo.first<<", "<< hijo.second<<") Costo: g(n) = " << gN <<"\n";
+
+    if(costo[hijo] > gN && parents[actual] != hijo){
+        costo[hijo] = gN;
+        cola.push({gN,hijo});   
+        parents[hijo] = actual;
+    }
+}
+
 void costoUniforme(std::vector<std::vector<int>> maze, std::pair<int,int> start, std::pair<int,int> goal){
     std::map<std::pair<int,int>, int> costo = llenarCostos();
     std::map<std::pair<int,int>, std::pair<int,int>> parents;
@@ -49,46 +68,48 @@ void costoUniforme(std::vector<std::vector<int>> maze, std::pair<int,int> start,
 
     while(!cola.empty()){
         std::pair<int,int> actual = cola.top().second;
+        cola.pop();
+
+        std::cout<<"\nNODO ACTUAL: ("<<actual.first<<", "<< actual.second<<")"<<" Costo: " <<  costo[actual]<<"\n";
+        std::cout<<"***********************HIJOS*****************************\n";
+        
         if(actual == goal){
             break;
         }
-        cola.pop();
 
         std::pair<int,int> arriba = {actual.first-1, actual.second};
         std::pair<int,int> abajo = {actual.first+1, actual.second};
         std::pair<int,int> derecha = {actual.first, actual.second+1};
         std::pair<int,int> izquierda = {actual.first, actual.second-1};
-        
-        if(arriba.first >-1 && costo[arriba] > costo[actual] + maze[arriba.first][arriba.second] && parents[actual] != arriba){
-            costo[arriba] = costo[actual] + maze[arriba.first][arriba.second];
-            cola.push({costo[arriba],arriba});   
-            parents[arriba] = actual;
-        }
-        if(abajo.first < 9 && costo[abajo] > costo[actual] + maze[abajo.first][abajo.second] && parents[actual] != abajo){
-            costo[abajo] = costo[actual] + maze[abajo.first][abajo.second];
-            cola.push({costo[abajo], abajo}); 
-            parents[abajo] = actual;
-        }
 
-        if(derecha.second < 15 && costo[derecha] > costo[actual] + maze[derecha.first][derecha.second] && parents[actual] != derecha){
-            costo[derecha] = costo[actual] + maze[derecha.first][derecha.second];
-            cola.push({costo[derecha], derecha});
-            parents[derecha] = actual;
-        }
-        if(izquierda.second > -1 && costo[izquierda] > costo[actual] + maze[izquierda.first][izquierda.second] && parents[actual] != izquierda){
-            costo[izquierda] = costo[actual] + maze[izquierda.first][izquierda.second];
-            cola.push({costo[izquierda], izquierda}); 
-            parents[izquierda] = actual;
-        }
+        generarHijoUniforme(arriba, actual, cola, costo, parents);
+        generarHijoUniforme(abajo, actual, cola, costo, parents);
+        generarHijoUniforme(derecha, actual, cola, costo, parents);
+        generarHijoUniforme(izquierda, actual, cola, costo, parents);
     }
 
-    std::cout<<"Costo TOTAl Uniforme: " << costo[goal]<<"\n";
+    std::cout<<"\nCosto TOTAl Uniforme: " << costo[goal]<<"\n";
     while(goal != start){
         std::cout<<"Costo: "<<costo[goal]<<" ("<<goal.first<< ", "<< goal.second<< ") -> \n"; 
         goal = parents[goal]; 
     }
     std::cout<<"("<< start.first<<" "<< start.second<<")\n"; 
+}
 
+void generarHijoAStar(std::pair<int,int>& hijo, std::pair<int,int>& actual, auto& cola, std::map<std::pair<int,int>, int>& costo, std::map<std::pair<int,int>, std::pair<int,int>>& parents ){
+    if(hijo.first<0|| hijo.first>8 || hijo.second>14 || hijo.second<0){
+        return;
+    }
+
+    int hN = abs(hijo.first - goal.first) + abs(hijo.second - goal.second);
+    int gN = costo[actual] + maze[hijo.first][hijo.second];
+    std::cout<<"("<<hijo.first<<", "<< hijo.second<<") Costo: f(n) = " << gN <<" + "<< hN <<" = "<< gN+ hN<<"\n";
+
+    if(costo[hijo] > gN && parents[actual] != hijo){
+        costo[hijo] = gN;
+        cola.push({gN+hN,hijo});   
+        parents[hijo] = actual;
+    }
 }
 
 void aStar(std::vector<std::vector<int>> maze, std::pair<int,int> start, std::pair<int,int> goal){
@@ -104,54 +125,28 @@ void aStar(std::vector<std::vector<int>> maze, std::pair<int,int> start, std::pa
         std::pair<int,int> actual = cola.top().second;
         cola.pop();
 
+        int gN = costo[actual];
+        int hN = abs(actual.first - goal.first) + abs(actual.second - goal.second);
+
+        std::cout<<"\nNODO ACTUAL: ("<<actual.first<<", "<< actual.second<<")"<<" Costo: " << gN <<" + "<< hN <<" = "<< gN+hN <<"\n";
+        std::cout<<"***********************HIJOS*****************************\n";
+        
+        if(actual == goal){
+            break;
+        }
+
         std::pair<int,int> arriba = {actual.first-1, actual.second};
         std::pair<int,int> abajo = {actual.first+1, actual.second};
         std::pair<int,int> derecha = {actual.first, actual.second+1};
         std::pair<int,int> izquierda = {actual.first, actual.second-1};
-        
-        if(arriba.first >-1){
-            int hN = abs(arriba.first - goal.first) + abs(arriba.second - goal.second);
-            int gN = costo[actual] + maze[arriba.first][arriba.second];
-            if(costo[arriba] > gN && parents[actual] != arriba){
-                costo[arriba] = gN;
-                cola.push({gN+hN,arriba});   
-                parents[arriba] = actual;
-            }
-        }
-        
 
-        if(abajo.first < 9){
-            int hN = abs(abajo.first - goal.first) + abs(abajo.second - goal.second);
-            int gN = costo[actual] + maze[abajo.first][abajo.second];
-            if(costo[abajo] > gN && parents[actual] != abajo){
-                costo[abajo] = gN;
-                cola.push({gN+hN, abajo}); 
-                parents[abajo] = actual;
-            }
-        }
-        
-        if(derecha.second < 15){
-            int hN = abs(derecha.first - goal.first) + abs(derecha.second - goal.second);
-            int gN = costo[actual]+ maze[derecha.first][derecha.second];
-            if(costo[derecha] > gN && parents[actual] != derecha){
-                costo[derecha] = gN;
-                cola.push({gN+hN, derecha});
-                parents[derecha] = actual;
-            }
-        }
-        
-        if(izquierda.second > -1){
-            int hN = abs(izquierda.first - goal.first) + abs(izquierda.second - goal.second);
-            int gN = costo[actual] + maze[izquierda.first][izquierda.second];
-            if(costo[izquierda] > gN && parents[actual] != izquierda){
-                costo[izquierda] =  gN;
-                cola.push({gN+hN, izquierda}); 
-                parents[izquierda] = actual;
-            }
-        }
+        generarHijoAStar(arriba, actual, cola, costo, parents);
+        generarHijoAStar(abajo, actual, cola, costo, parents);
+        generarHijoAStar(derecha, actual, cola, costo, parents);
+        generarHijoAStar(izquierda, actual, cola, costo, parents);
     }
 
-    std::cout<<"Costo TOTAl A*: " << costo[goal]<<"\n";
+    std::cout<<"\nCosto TOTAl A*: " << costo[goal]<<"\n";
     while(goal != start){
         std::cout<<"Costo: "<<costo[goal]<<" ("<<goal.first<< ", "<< goal.second<< ") -> \n"; 
         goal = parents[goal]; 
@@ -164,24 +159,22 @@ int main(){
 
     struct timeval startTime, stop;
     double secs = 0;
-    std::pair<int,int> goal = {8,0}; //8,0
-    std::pair<int,int> start = {7,12}; //7,12
-    std::vector<std::vector<int>> maze = llenarLaberinto();
+    maze = llenarLaberinto();
 
-
+    
     gettimeofday(&startTime, NULL);
     costoUniforme(maze, start, goal);
 
     gettimeofday(&stop, NULL);
     secs = (double)(stop.tv_usec - startTime.tv_usec) / 1000000 + (double)(stop.tv_sec - startTime.tv_sec);
-    printf("\ntiempo de cálculo %f\n", secs);
+    printf("\nTiempo de Cálculo Algoritmo Uniforme: %f\n", secs);
     
-
+    std::cout<<"\n\n";
     gettimeofday(&startTime, NULL);
     aStar(maze, start, goal);
 
     gettimeofday(&stop, NULL);
     secs = (double)(stop.tv_usec - startTime.tv_usec) / 1000000 + (double)(stop.tv_sec - startTime.tv_sec);
-    printf("\ntiempo de cálculo %f\n", secs);
-
+    printf("\nTiempo de Cálculo Algoritmo A*: %f\n", secs);
+    
 }
